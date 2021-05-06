d1b29a57785d848c96f3213f45ba190afce62927
henrycai <alaba.havai@gmail.com>
2021-05-06 23:17:17 +0800

    Java and Kotlin variable,data type

--- src/main/kotlin/com/tom/Hello.kt
+++ src/main/kotlin/com/tom/Hello.kt
@@ -3,8 +3,15 @@ package com.tom
 fun main() {
 //    println("Hello Kotlin");
 //    Human().hello()
+    //版本控制視窗 command + 9
     val h = Human()
     h.hello()
+
+    var age:Int = 25;
+    age = age.plus(1)
+    val weight =65.2
+    val control:Boolean = true
+    print("age = "+age)
 }
 class Human{
 
